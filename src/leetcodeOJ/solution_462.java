c365df8b2f9dd6fd18583db96b8ca6838c0b89d7
fangjinxia <fangjinxia@iie.ac.cn>
2017-10-26 17:02:18 +0800

    Minimum Moves to Equal Array Elements II

---
+++ src/leetcodeOJ/solution_462.java
@@ -0,0 +1,31 @@
+package leetcodeOJ;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+//LeetCodeOJ 461
+public class solution_462 {
+	//My AC code
+	public static int minMoves2(int[] nums) {
+		///<找到数组的中值
+		
+	}
+	
+	public static void main(String[] args){
+//		//���ܴӼ����������ֵ
+//		Scanner s = new Scanner(System.in);
+//		
+//		//����һ��int����������
+//		Integer x = s.nextInt();
+//		Integer y = s.nextInt();
+//		//����һ��double����������
+//		//Double d = s.nextDouble(); 
+//		
+//		//����һ��string����������
+//		//String str = s.next();
+//		
+//		int res = hammingDistance(x, y);
+//		System.out.println(res);
+	}
+}
