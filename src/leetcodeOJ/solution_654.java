9bc52f1f8515c05cfd62c234639f5a117a0c5947
fangjinxia <fangjinxia@iie.ac.cn>
2017-10-26 15:54:30 +0800

    Maximum Binary Tree

--- src/leetcodeOJ/solution_654.java
+++ src/leetcodeOJ/solution_654.java
@@ -6,30 +6,35 @@ import leetcodeOJ.TreeNode;
 
 public class solution_654 {
 	 /*
-	 * dp
 	 */
-	protected static int minSteps(int n) {
-		//using two pointer
-		int[] dp = new int[n+1];
-		for(int i = 0; i <= n; ++i) {
-			dp[i] = i;
-		}
-		for(int i = 2; i <= (n>>1); ++i) {
-			int k = 2;
-			for(int j = 2*i; j <= n; j += i) {
-				dp[j] = Math.min(dp[j], dp[i]+k);
-				k++;
+	protected static TreeNode constructMaximumBinaryTree(int[] nums) {
+		int n = nums.length;
+		if(n == 0)
+			return null;
+		return getTree(nums, 0, n-1);
+	}
+	
+	private static TreeNode getTree(int[] nums, int start, int end) {
+		if(start > end)
+			return null;
+		///<find the index of the maximum number
+		int idx = start;
+		for(int i = start+1; i <= end; ++i) {
+			if(nums[i] > nums[idx]) {
+				idx = i;
 			}
 		}
 		
-		return dp[n];
+		TreeNode node = new TreeNode(nums[idx]);
+		node.left = getTree(nums, start, idx-1);
+		node.right = getTree(nums, idx+1, end);
+		
+		return node;
 	}
 	
-	
 	public static void main(String[] args) {
-		int i = 9;
-		
-		System.out.println(minSteps(i));
+//		int i = 9;
+//		
 	}
 }
  
