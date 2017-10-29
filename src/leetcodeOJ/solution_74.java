45b43e6dae35d938d49dfe8a968f8c36c58445fc
fangjinxia <fangjinxia@iie.ac.cn>
2017-10-29 16:19:39 +0800

    Search a 2D Matrix

---
+++ src/leetcodeOJ/solution_74.java
@@ -0,0 +1,43 @@
+package leetcodeOJ;
+
+import java.util.*;
+
+public class solution_74 {
+	public static boolean searchMatrix(int[][] matrix, int target) {
+		int n = matrix.length;
+		if(n == 0)
+			return false;
+		int m = matrix[0].length;
+		
+		int i = 0, j = m-1;
+		while(true){
+			if(i >= n || j < 0)
+				break;
+			if(matrix[i][j] == target){
+				return true;
+			}
+			else if(matrix[i][j] > target){
+				--j;
+			}
+			else{
+				++i;
+			}
+		}
+		
+		return false;
+	}
+	
+	
+	public static void main(String[] args){
+//		int[] nums = {1, 2, 3, 4, 5, 6, 7};
+//		int target = 12;
+//		
+//		int[] result = new int[2]; 
+//		result = twoSum(nums, target);
+//		
+//		String a = String.valueOf(2);   //integer to numeric string
+//		int i = Integer.parseInt(a); //numeric string to an int
+//		
+//		System.out.println(Arrays.toString(result));
+	}
+}
