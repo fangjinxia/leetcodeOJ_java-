c1254bfb640ee1ba27002f93a4949add949d5a51
fangjinxia <fangjinxia@iie.ac.cn>
2017-11-07 09:50:21 +0800

    215. Kth Largest Element in an Array

--- src/leetcodeOJ/solution_215.java
+++ src/leetcodeOJ/solution_215.java
@@ -11,6 +11,44 @@ public class solution_215 {
 		Arrays.sort(nums);
 		return nums[nums.length - k];
 	}
+	
+	///<another method
+	private static int findkth(int[] nums, int k) {
+		int n = nums.length;
+		if(nums == null || n == 0)
+			return -1;
+		
+		return kth(nums, 0, n-1, k);
+	}
+	
+	private static int kth(int[] nums, int left, int right, int k) {
+		if(left >= right) {
+			return nums[right];
+		}
+		
+		int m = left;
+		for(int i = left+1; i <= right; ++i) {
+			if(nums[i] > nums[left]) {
+				++m;
+				swap(nums, i, m);
+			}
+		}
+		
+		swap(nums, m, left);
+		if(k == m+1)
+			return nums[m];
+		else if(k > m+1)
+			return kth(nums, m+1, right, k);
+		else
+			return kth(nums, left, m-1, k);
+	}
+	
+	public static void swap(int[] nums, int i, int j) {
+    	int tmp = nums[i];
+    	nums[i] = nums[j];
+    	nums[j] = tmp;
+    }
+	
 	public static void main(String[] args) {
 		//String[] strs = {"Hello","Alaska","Dad","Peace"};
 		//String[] res = findWords(strs);
