a746b441e74555e3c82f05aa446b0f1f84b31bf4
fangjinxia <fangjinxia@iie.ac.cn>
2017-11-22 20:40:49 +0800

    N-Queues问题

---
+++ src/leetcodeOJ/solution_51.java
@@ -0,0 +1,72 @@
+package leetcodeOJ;
+
+import java.util.*;
+
+public class solution_51 {
+	public static List<List<String>> solveNQueens(int n) {
+		List<List<String>> res = new LinkedList<>();
+		if(n <= 0)
+			return res;
+		List<Integer> cur = new LinkedList<>();
+		
+		backtracing(res, cur, n, 0);
+		
+		return res;
+	}
+	
+	private static void backtracing(List<List<String>> res, List<Integer> cur, int n, int item) {
+		if(item == n) {
+			res.add(drawBoard(cur));
+			return ;
+		}
+		
+		for(int j = 0; j < n; ++j) {
+			//当前位置可以放Queue
+			if(isValid(cur, j)) {
+				 cur.add(j);
+				 backtracing(res, cur, n, item+1);
+				 cur.remove(cur.size()-1);
+			}
+		}
+		
+		
+	}
+	
+	
+	private static Boolean isValid(List<Integer> list, int curCol) {
+		//int[][] dir = {{1,0}, {-1,0}, {0,1}, {0,-1}, {1,-1}, {1,1}, {-1,1}, {-1,-1}};
+		int curRow = list.size();
+		int curSum = curRow + curCol;
+		int curDiff = curRow - curCol;
+		for(int r = 0; r < curRow; ++r) {
+			int c = list.get(r);
+			int sum = r+c;
+			int diff = r-c;
+			if(curCol == c || sum == curSum || diff == curDiff)
+				return false;
+		}
+		
+		
+		return true;
+	}
+	
+	private static List<String> drawBoard(List<Integer> list){
+		List<String> res = new ArrayList<String>();
+		int n = list.size();
+		for(int i = 0; i < n; ++i) {
+			StringBuilder str = new StringBuilder();
+			for(int j = 0; j < n; ++j) {
+				str.append((j == list.get(i)) ? 'Q' : '.');
+			}
+			res.add(str.toString());
+		}
+		
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		
+		List<List<String>> res = solveNQueens(4);
+		System.out.println(res);
+	}
+}
