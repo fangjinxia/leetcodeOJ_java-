a6aa11271d51abfaf7d204d1259a2c7095a29e83
fangjinxia <fangjinxia@iie.ac.cn>
2017-11-25 10:28:12 +0800

    Invert Binary Tree

---
+++ src/leetcodeOJ/solution_226.java
@@ -0,0 +1,21 @@
+package leetcodeOJ;
+
+import java.util.*;
+import leetcodeOJ.TreeNode;
+
+public class solution_226 {
+	public TreeNode invertTree(TreeNode root) {
+		if(root == null)
+			return root;
+		
+		TreeNode tmp = root.left;
+		root.left = root.right;
+		root.right = tmp;
+		
+		invertTree(root.left);
+		invertTree(root.right);
+		
+		return root;
+	}
+	
+}
