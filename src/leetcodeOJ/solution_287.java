d84c1ab6134371437d5a73c7199c4402b97913d5
fangjinxia <fangjinxia@iie.ac.cn>
2017-12-14 15:02:33 +0800

    287. Find the Duplicate Number

---
+++ src/leetcodeOJ/solution_287.java
@@ -0,0 +1,33 @@
+package leetcodeOJ;
+
+import java.util.*;
+import java.lang.String;
+import leetcodeOJ.TreeNode;
+
+public class solution_287 {
+	/*
+	 */
+	public int findDuplicate(int[] nums) {
+		int n = nums.length;
+		if(n == 0)
+			return -1;
+		
+		int lo = 1; 
+		int hi = n-1;
+		while(lo <= hi) {
+			int mid = lo + (hi-lo)/2;
+			int cnt = 0;
+			for(int it : nums) {
+				if(it <= mid)
+					++cnt;
+			}
+			if(cnt <= mid)
+				lo = mid+1;
+			else
+				hi = mid-1;
+		}
+		
+		return lo;
+	}
+}
+ 
